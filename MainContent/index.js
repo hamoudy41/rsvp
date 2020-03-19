96f5219f83cbc60f018e674936d2533e3d675ac0
hamoudy41 <hamoudy41@yahoo.com>
2020-03-19 11:30:03 +0100

    first commit

---
+++ MainContent/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Counter from './Counter';
+import GuestList from './GuestList';
+import PropTypes from 'prop-types';
+import ConfirmedFilter from './ConfirmedFilter';
+
+const MainContent = props => {
+  return (
+    <div className="main">
+<ConfirmedFilter
+ toggleFilter = {props.toggleFilter}
+ isFiltered = {props.isFiltered} />
+<Counter
+  totalInvited={props.totalInvited}
+  numberAttending={props.numberAttending}
+  numberUnconfirmed={props.numberUnconfirmed}
+/>
+<GuestList
+  guests={props.guests}
+  toggleConfirmationAt={props.toggleConfirmationAt}
+  toggleEditingAt={props.toggleEditingAt}
+  setNameAt={props.setNameAt}
+  isFiltered={props.isFiltered}
+  removeGuestAt={props.removeGuestAt}
+  pendingGuest={props.pendingGuest}
+/>
+</div>
+  )
+}
+
+MainContent.propTypes = {
+  toggleFilter: PropTypes.func.isRequired,
+  isFiltered: PropTypes.bool.isRequired,
+  numberAttending: PropTypes.number,
+  numberUnconfirmed: PropTypes.number,
+  totalInvited: PropTypes.number,
+  guests: PropTypes.array.isRequired,
+  toggleConfirmationAt: PropTypes.func.isRequired,
+  toggleEditingAt: PropTypes.func.isRequired,
+  setNameAt: PropTypes.func.isRequired,
+  removeGuestAt: PropTypes.func.isRequired,
+  pendingGuest: PropTypes.string.isRequired
+}
+
+
+export default MainContent;
