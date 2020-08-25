29516e26dddb2b769f1da0d39921fe49f2d49159
hamoudy41 <hamoudy41@yahoo.com>
2020-08-25 15:00:59 +0200

    reverse changes

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,6 +8,7 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
     "@storybook/addon-actions",
-    "@storybook/addon-docs"
+    "@storybook/addon-docs",
+    "@storybook/addon-console"
   ]
 }
