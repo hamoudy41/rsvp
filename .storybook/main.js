57f1383df78911b2a69ad3a2f39ecfbc401704cf
hamoudy41 <hamoudy41@yahoo.com>
2020-08-25 19:24:26 +0200

    packages fix

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,7 +8,7 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/preset-create-react-app",
     "@storybook/addon-actions",
-    "@storybook/addon-docs",
-    "@storybook/addon-console"
+    "@storybook/addon-console",
+    "@storybook/addon-docs"
   ]
 }
